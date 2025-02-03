25266d3a6f51bf9f7d9a6c7567c634ef54eedd9d
Tina Wulff <tina@tinawulff.dk>
2025-02-03 09:04:34 +0100

    opgave1 done

--- arrays.js
+++ arrays.js
@@ -5,11 +5,34 @@ document.addEventListener("DOMContentLoaded", function() {
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
-
-    let newNumbers = [21, 26, 32, 45, 48, 55]
+/*
+    let newNumbers = numbersArray.filter(num => num > 20)
     console.log("Opgave 1", newNumbers)
+*/
 
-    
+        function bigNumbers(value) {
+            return value > 20;
+        }
+
+        const newNumbers = numbersArray.filter(bigNumbers);
+
+        console.log("Opgave 1", newNumbers);
+
+/*  Mdn's eksempel:
+
+function isBigEnough(value) {
+    return value >= 10;
+  }
+  
+  const filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
+  // filtered is [12, 130, 44]
+*/
+
+
+
+
+
+
 }) // ends DOMContentLoaded
 
 
