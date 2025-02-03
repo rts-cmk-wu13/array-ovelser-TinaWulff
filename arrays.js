952e0e85b1866a9dd44f40207526522270740113
Tina Wulff <tina@tinawulff.dk>
2025-02-03 08:43:54 +0100

    begynd Opg1

--- arrays.js
+++ arrays.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
+
     let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
     let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
-    // --- Løs opgaverne herunder --- 
 
+    let newNumbers = [21, 26, 32, 45, 48, 55]
+    console.log("Opgave 1", newNumbers)
+
+    
 }) // ends DOMContentLoaded
+
+
+
