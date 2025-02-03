bb11965dbdf119366e546976692aaa6091a27e14
Tina Wulff <tina@tinawulff.dk>
2025-02-03 09:31:10 +0100

    til og med opg 4a

--- arrays.js
+++ arrays.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
-    let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
+ 
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
+
+
+    /// OPGAVE 1
 /*
     let newNumbers = numbersArray.filter(num => num > 20)
     console.log("Opgave 1", newNumbers)
@@ -27,6 +30,66 @@ function isBigEnough(value) {
   const filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
   // filtered is [12, 130, 44]
 */
+
+
+// OPGAVE 2
+
+catsArray.push('Sniffles');
+console.log("Opgave 2", catsArray);
+
+
+// OPGAVE 3
+
+/* 
+Opgave 3a
+Check om der i dogsArray er en hund som hedder "Trixie".
+
+Udskriv "true" eller "false" i konsollen afhængig af svaret.
+
+console.log("Opgave 3a", ...)
+
+Opgave 3b
+Check om der i dogsArray er en hund som hedder "Baxter".
+
+Udskriv "true" eller "false" i konsollen afhængig af svaret.
+
+console.log("Opgave 3b", ...)
+
+*/
+
+// OPGAVE 3a
+
+let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
+
+console.log("opgave 3a " + (dogsArray.includes('Trixie')));
+
+// OPGAVE 3b
+
+console.log("opgave 3b " + (dogsArray.includes('Baxter')));
+
+
+// OPGAVE 4a
+
+//TEST NEDENSTÅENDE
+console.log("opgave 4prøve " + (catsArray.includes('Bagheera')));
+
+
+const result = catsArray.find(({ name }) => name === "Bagheera");
+
+console.log("opgave 4a", result);
+
+// OPGAVE 4b
+
+
+
+
+
+
+
+
+
+
+
 
 
 
